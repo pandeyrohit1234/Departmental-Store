158e82c374a1f60e34dba942be02ef793e6a36f8
Rohit kumar pandey <80757398+pandeyrohit1234@users.noreply.github.com>
2021-05-22 08:56:34 +0530

    UpdatedDepartmentalStore

---
+++ NewDepartmentalStore/DepStore.UI/Program.cs
@@ -0,0 +1,30 @@
+﻿using ProductContext;
+using ProductTable;
+using System;
+using ProductTable;
+using System.Linq;
+using System.Collections.Generic;
+using OperationOnDatabase;
+
+namespace ProductCatalog
+{
+     class Program
+    {
+
+       
+
+
+
+        static void Main(string[] args)
+        {
+
+               MainMenu.Menu();
+
+
+
+        }
+       
+    }
+}
+
+
